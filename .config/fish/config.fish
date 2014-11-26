36bdee38a628e00b55ac68ddbda3f91593350491
Michael Tolly <miketolly@gmail.com>
2014-11-26 15:46:33 -0600

    Fix fish variables
    The empty entries in MANPATH ensure that the exported
    variable starts with a colon, see
    https://github.com/fish-shell/fish-shell/issues/935

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,8 +3,11 @@ set fish_greeting
 
 # PATH
 
+# Use homebrew stuff before pre-installed stuff
+set PATH /usr/local/bin $PATH
+
 # GHC from http://ghcformacosx.github.io/
-set GHC_DOT_APP /Applications/ghc-7.8.3.app
+set -x GHC_DOT_APP /Applications/ghc-7.8.3.app
 set PATH $HOME/.cabal/bin $GHC_DOT_APP/Contents/bin $PATH
 
 # GHCJS binaries
@@ -20,11 +23,15 @@ set PATH $HOME/bin $HOME/bin/.cabal-sandbox/bin $PATH
 set PATH (brew --prefix homebrew/php/php55)/bin $PATH
 
 # Android SDK
-set ANDROID_HOME /usr/local/opt/android-sdk
+set -x ANDROID_HOME /usr/local/opt/android-sdk
 
 # MacPorts
 set PATH /opt/local/bin /opt/local/sbin $PATH
-set MANPATH /opt/local/share/man $MANPATH
+set -x MANPATH "" /opt/local/share/man $MANPATH
+
+# Nethack
+set PATH /usr/games/bin/ $PATH
+set -x MANPATH "" /usr/games/man $MANPATH
 
 # Aliases
 alias gs "git status"
