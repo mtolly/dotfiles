afc4b3b1ce73c656078c80bfba5ebc25da9e98ea
Michael Tolly <miketolly@gmail.com>
2016-10-25 20:48:36 -0500

    Updates

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,73 +1,21 @@
-# Greeting mesage
-set fish_greeting
-
-# PATH
-
-# Use homebrew stuff before pre-installed stuff
-set PATH /usr/local/bin $PATH
-
-# GHC from http://ghcformacosx.github.io/
-set -x GHC_DOT_APP /Applications/ghc-7.10.2.app
-set PATH $HOME/.cabal/bin $GHC_DOT_APP/Contents/bin $PATH
-
-# GHCJS binaries
-set PATH $HOME/ghcjs/.cabal-sandbox/bin $PATH
-
-# subl
 set PATH "/Applications/Sublime Text.app/Contents/SharedSupport/bin" $PATH
 
-# my bin/ folder + sandboxed hs programs
-set PATH $HOME/bin $HOME/bin/.cabal-sandbox/bin $PATH
-
-# Use Homebrew's PHP
-# set PATH (brew --prefix homebrew/php/php55)/bin $PATH
-
-# Android SDK
-set -x ANDROID_HOME /Users/mtolly/Library/Android/sdk/
-
-# MacPorts
-set PATH /opt/local/bin /opt/local/sbin $PATH
-set -x MANPATH "" /opt/local/share/man $MANPATH
-
-# Nethack
-set PATH /usr/games/bin/ $PATH
-set -x MANPATH "" /usr/games/man $MANPATH
-
-# LilyPond
-set PATH /Applications/LilyPond.app/Contents/Resources/bin $PATH
-
-# for jammittools & onyxbuild
-set -x JAMMIT "$HOME/Downloads/music (on passport)/Jammit (on passport)"
-
-# devkitpro
-set -x DEVKITPRO /opt/devkitpro
-set -x DEVKITARM $DEVKITPRO/devkitARM
-set PATH $PATH $DEVKITARM/bin
-
-# mentioned in byobu homebrew caveats
-set -x BYOBU_PREFIX (brew --prefix)
-
-set -x PKG_CONFIG_PATH /opt/X11/lib/pkgconfig
-set -x C_INCLUDE_PATH /opt/X11/include
-
-# stack-installed programs
 set PATH $HOME/.local/bin $PATH
 
-# Android tools
-set PATH $HOME/Library/Android/sdk/platform-tools $PATH
-set PATH $HOME/Library/Android/sdk/tools $PATH
-set PATH $HOME/Library/Android/sdk/build-tools/22.0.1 $PATH
+set PATH $HOME/.stack/programs/x86_64-osx/ghc-8.0.1/bin/ $PATH
 
-# Go
-set -x GOPATH $HOME/go
-set PATH /usr/local/opt/go/libexec/bin $PATH
+alias git hub
 
-# Aliases
 alias gs "git status"
-alias cri "cordova run ios"
-alias cra "cordova run android"
-alias cid "cabal install --only-dependencies"
-alias cb "cabal build"
-alias cbjs "cabal configure --ghcjs; and cabal build"
 alias lilyrepl "rlwrap lilypond scheme-sandbox"
-alias lx "exa -lh --git"
+
+alias onyx "onyx --color --audio /Users/mtolly/Passport"
+
+set PATH $HOME/mac_android/android-ndk-r12b $PATH
+set PATH $HOME/mac_android/android-sdk-macosx/platform-tools $PATH
+set -x JAVAHOME (/usr/libexec/java_home)
+set -x ANDROID_HOME $HOME/mac_android/android-sdk-macosx
+
+function .
+  open .
+end
--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,53 +1,19 @@
-# simple port of bira.zsh-theme
-
-function _git_branch_name
-  echo (command git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
-end
-
-function _git_is_dirty
-  echo (command git status -s --ignore-submodules=dirty ^/dev/null)
-end
-
-function fish_prompt --description 'Write out the prompt'
-
-  echo -n "╭─"
-  set_color $fish_color_user
-  echo -n (whoami)
-  set_color normal
-  echo -n @
-  set_color $fish_color_host
-  echo -n (hostname -s)
-
-  echo -n "  "
-  set_color $fish_color_cwd
+function fish_prompt
+  set the_status $status
+  echo -n "["
+  set_color purple
   echo -n (prompt_pwd)
-
-  if [ (_git_branch_name) ]
-    set_color yellow
-    set -l git_branch (_git_branch_name)
-    if [ (_git_is_dirty) ]
-      set git_info '<'$git_branch"*"'>'
-    else
-      set git_info '<'$git_branch'>'
-    end
-    echo -n "  $git_info"
-  end
-
-  echo
-
   set_color normal
-  echo -n "╰─\$ "
-
-end
-
-function fish_right_prompt
-  set -l last_status $status
-
-  if not test $last_status -eq 0
-    # todo: right-justify
-    set_color $fish_color_error
-    echo "$last_status ↵ "
+  echo -n "]"
+  set the_git_status (git status --porcelain 2> /dev/null)
+  if test "$the_git_status" != ""
+    echo -n "*"
+  end
+  if test "$the_status" -ne 0
+    set_color red
+    echo -n "! "
     set_color normal
-    echo
+  else
+    echo -n ": "
   end
 end
