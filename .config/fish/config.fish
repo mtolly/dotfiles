085f7e7a5206e8191557446db821b9192f790326
Michael Tolly <miketolly@gmail.com>
2024-01-06 13:55:28 -0600

    Updates

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,6 +7,9 @@ end
 if test -d $HOME/.local/bin
   set PATH $HOME/.local/bin $PATH
 end
+if test -d $HOME/.cargo/bin
+  set PATH $HOME/.cargo/bin $PATH
+end
 set EDITOR 'subl --wait'
 
 alias git hub
@@ -27,3 +30,7 @@ end
 set -x SSH_AUTH_SOCK $XDG_RUNTIME_DIR/ssh-agent.socket
 
 set fish_greeting
+
+if status is-interactive
+  keychain --eval /home/mtolly/.ssh/id_rsa --quiet | source
+end
